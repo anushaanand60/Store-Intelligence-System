1277912713b0d9db4151a871a9b4befd97c64a3e
anusha anand <anandanusha610@gmail.com>
2026-06-02 00:24:06 +0530

    added iou calculation and active track helper functions

--- worker/tracker.py
+++ worker/tracker.py
@@ -13,6 +13,9 @@ class SpatialHashTracker:
         self.tracks = {}
         self.next_track_id = 1
         self.max_stale = 30
+    
+    def get_active_count(self):
+        return len(self.tracks)
 
     def _cell(self, bbox):
         x1, y1, x2, y2 = bbox
@@ -21,3 +24,15 @@ class SpatialHashTracker:
         row = min(max(cy // self.cell_h, 0), self.grid_rows - 1)
         col = min(max(cx // self.cell_w, 0), self.grid_cols - 1)
         return row, col
+
+    @staticmethod
+    def _iou(a, b):
+        x1 = max(a[0], b[0])
+        y1 = max(a[1], b[1])
+        x2 = min(a[2], b[2])
+        y2 = min(a[3], b[3])
+        inter = max(0.0, x2 - x1) * max(0.0, y2 - y1)
+        area_a = max(0.0, (a[2] - a[0])) * max(0.0, (a[3] - a[1]))
+        area_b = max(0.0, (b[2] - b[0])) * max(0.0, (b[3] - b[1]))
+        union = area_a + area_b - inter + 1e-6
+        return inter / union
